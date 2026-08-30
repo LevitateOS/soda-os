937d4a63b69f006fcea577481472247e50f27d6b
Vince Liem <vincepaul.liem@gmail.com>
2026-08-31 00:27:42 +0200

    Revert "refactor: remove interrupted provisioning job reconciliation logic from startup sequence"
    This reverts commit b01ce216b7347da27990bc33d208a496536bac76.

--- cmd/sodad/main.go
+++ cmd/sodad/main.go
@@ -36,6 +36,13 @@ func run(logger *slog.Logger) error {
 	if err != nil {
 		return err
 	}
+	interrupted, err := persistence.FailInterruptedProvisioning(runContext)
+	if err != nil {
+		return err
+	}
+	if interrupted != 0 {
+		logger.Warn("marked interrupted provisioning jobs failed", slog.Int64("jobs", interrupted))
+	}
 	system := host.New(projects)
 	observer, err := telemetry.NewManager(telemetry.NewSystemHostSampler(nil, nil))
 	if err != nil {
